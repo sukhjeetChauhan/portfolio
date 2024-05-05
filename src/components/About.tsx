6dc08846171b6d083b81aded5b1137cc62633ec2
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-05-05 12:33:17 +1200

    add some styling

---
+++ src/components/About.tsx
@@ -0,0 +1,30 @@
+import '../styles/About.css'
+
+export default function About() {
+  return (
+    <section className="about">
+      <div className="about-heading">
+        <h2>About me</h2>
+      </div>
+      <div className="about-image">
+        <img src="/images/index/mainProPicCopy.JPG" alt="image of self" />
+      </div>
+      <div className="about-description">
+        <p>
+          My career aspirations are to become an experienced Full-Stack Web
+          developer. I believe I can reach that point by taking small steps and
+          self-improving every day. I wish to be part of a team which makes a
+          difference to the society, even if their contribution is minimal. I
+          have a graduate and Post-graduate qualifications in the field of IT
+          and business and I am recent graduate from Dev Academy Aotearoa
+        </p>
+        <p>
+          Since past year, I have been diving deeper into the web development
+          world and am really keen to begin my career in this field. I have few
+          projects under my belt which I have displayed in the following
+          section.
+        </p>
+      </div>
+    </section>
+  )
+}
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,17 @@
 import '../styles/App.css'
+import About from './About'
 import Landing from './Landing'
 import Navigation from './Navigation'
 import Projects from './Projects'
 
 function App() {
   return (
-    <>
+    <div className="section">
       <Navigation />
       <Landing />
+      <About />
       <Projects />
-    </>
+    </div>
   )
 }
 
